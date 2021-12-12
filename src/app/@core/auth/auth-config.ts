698fa06be630f681821c94cf2f72d1a30db57786
MassimoSCHIBECI <m.schibeci.esterno@almaviva.it>
2021-12-12 12:16:16 +0100

    modified carriage return

--- src/app/@core/auth/auth-config.ts
+++ src/app/@core/auth/auth-config.ts
@@ -2,12 +2,21 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 import { environment } from '@env/environment';
 
 export const authConfig: AuthConfig = {
+  // Url of the Identity Provider
   issuer: environment.Oidc_Issuer,
+  // The SPA's id. The SPA is registerd with this id at the auth-server
   clientId: environment.Oidc_ClientId,
+  // Login Url of the Identity Provider
+  // loginUrl: 'https://demo.identityserver.com/identity/connect/authorize',
+  // Login Url of the Identity Provider
+  // logoutUrl: 'https://demo.identityserver.com/identity/connect/endsession',
   responseType: environment.Oidc_responseType,
+  // URL of the SPA to redirect the user to after login
   redirectUri: environment.Oidc_redirectUri,
   postLogoutRedirectUri: environment.Oidc_postLogoutRedirectUri,
   silentRefreshRedirectUri: environment.Oidc_silentRefreshRedirectUri,
+  // set the scope for the permissions the client should request
+  // The first three are defined by OIDC. Also provide user sepecific
   scope: environment.Oidc_scope,
   useSilentRefresh: environment.Oidc_useSilentRefresh,
   silentRefreshTimeout: environment.Oidc_silentRefreshTimeout,
